0faecb146d19bf233405cb76ce30153a2d40548c
Joanavel Pascual <joanavelpascual@pursuit.org>
2023-04-04 20:17:09 -0400

    Saving Progress

--- main.js
+++ main.js
@@ -7,7 +7,21 @@
 // To ensure Cypress tests work as expeded, add any code/functions that you would like to run on page load inside this function
 
 function run() {
- // Add code you want to run on page load here
+    fetch("https://resource-ghibli-api.onrender.com/films")
+    .then(response => response.json())
+    .then(data => {
+        const select = document.getElementById("titles");
+        data.forEach( movie => {
+            const option = document.createElement("option");
+            option.value = movie.id;
+            option.text = movie.title;
+            select.appendChild(option);
+      
+        });
+    })
+    .catch(error => {
+        console.error(error)
+    })
 }
 
 // This function will "pause" the functionality expected on load long enough to allow Cypress to fully load
